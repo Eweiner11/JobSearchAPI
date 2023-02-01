cdc8ce9e017e3a0533dd6fb2a312a9cfd64b196c
Eric Weiner <Eweiner11@gmail.com>
2023-02-01 01:00:58 -0500

    updated env

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 
 const connectDB = async () => {
-  await mongoose.connect(`mongodb://localhost:27017/jobsearch`, {
+  await mongoose.connect(process.env.MONGO_CONNECTION_URI, {
     useNewUrlParser: true,
   });
 
