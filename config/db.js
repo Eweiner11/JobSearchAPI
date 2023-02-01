a5ceb738ab6965dc79896861f6b79c8cd1010e08
EricWeiner <Eweiner11@gmail.com>
2023-01-31 22:19:03 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,13 +3,11 @@ mongoose.set('strictQuery', true);
 
 const connectDB = async () => {
   try{
-
     await mongoose.connect(process.env.DATABASE_CONNECTION);
     console.log('MongoDB Connected!');
   }catch(err){
     console.log(err)
   }
-console.log('done')
 };
 
 module.exports = connectDB;
